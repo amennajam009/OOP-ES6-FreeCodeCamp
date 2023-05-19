14e411be11ff51a2a9cd643289b59b7c9fb7eeaf
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-19 13:28:27 +0500

    add

---
+++ ES6/DestructingAssignment.js
@@ -0,0 +1,10 @@
+//Destructing assignment humra ek special type ka syntax hai jisy hum directly object may sy he values pakr saktay han 
+
+
+const MyObj = {
+    name:'amen',
+    Today:'19th may 2023',
+    temperature:'Hot'
+}
+const {Today,temperature} = MyObj;
+console.log(MyObj);
