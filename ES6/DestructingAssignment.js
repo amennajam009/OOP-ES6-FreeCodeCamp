4dad077814182062aea8f893616ac003aaeec728
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-19 14:43:39 +0500

    add

--- ES6/DestructingAssignment.js
+++ ES6/DestructingAssignment.js
@@ -8,3 +8,42 @@ const MyObj = {
 }
 const {Today,temperature} = MyObj;
 console.log(MyObj);
+
+//Destructing Values from nested Object 
+const MySecondObj = {
+    Name:'amen',
+    Class:'university',
+    TodayWeather:'Hot',
+    TomorrowWeather:'Sunny'
+}
+const {TodayWeather:{TodayWeather:low , TomorrowWeather:high}}=MySecondObj
+console.log(MySecondObj);
+
+//Nested One 
+const MyObjNo2 = {
+    myinfo:{
+        name:'amen',
+        class:'13th',
+        phone:'090078601'
+    },
+    fatherInfo:{
+        fathername:'najam',
+        fathersurname:'william',
+        phone:'08968565',
+        country:'pakistan'
+    },
+    brotherInfo:{
+        name:'string',
+        education:"string",
+        phone:'3456356466',
+        country:'pakistan',
+        city:'lahore',
+        greet:function(){
+          return "hello my name is " + this.name;
+        }
+    }
+}
+const {name:{name:Allname, education:Alleducation}}= MyObjNo2;
+console.log(MyObjNo2.brotherInfo.greet());
+console.log(Allname);
+console.log(Alleducation);
