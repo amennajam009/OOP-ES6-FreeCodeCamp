86fb2e448c02eb6739c20dc68a6d3770ad47ef95
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-19 15:10:02 +0500

    add

---
+++ ES6/SwapValuesByDestructing.js
@@ -0,0 +1,11 @@
+// With the use of Destructing We can swap Our array values 
+
+const MyValues = [fruits = ['bananana', 'apple','mango','grapes'], numbers= [ 1,2,3,4,5,6,7] ];
+[fruits,numbers] = [numbers,fruits];
+//now the output will be the array of number will be in index 0 and fruits object will be on 1 index
+
+const MyFruits = ['bananan', 'apple','mango', 'grapes', 'strawberry'];
+//now i want to reverse this whole array
+[...MyFruits].reverse();
+
+
