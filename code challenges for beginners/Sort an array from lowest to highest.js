24939a5e37654f73cc260dc27bee6b0305f2f22e
trench009 <amennajam7@gmail.com>
2023-05-23 02:15:53 +0500

    add

--- code challenges for beginners/Sort an array from lowest to highest.js
+++ code challenges for beginners/Sort an array from lowest to highest.js
@@ -3,3 +3,9 @@
 const MyArr = [5,6,7,4,3,2,1,7,9,8];
 const arr = MyArr.sort((a,b) => a-b)
 console.log(arr)
+
+
+//again
+const Values = [33,55,77,88,1,3,4,5,2,6,,8,9,5];
+const value2 = Values.sort((a,b) => a-b)
+console.log(value2)
