de1a712c0f335e0cbdd115aa7fa20e6bc8c9c1a0
trench009 <amennajam7@gmail.com>
2023-05-23 02:00:25 +0500

    add

---
+++ code challenges for beginners/Create a function that reverses an array.js
@@ -0,0 +1,12 @@
+// create a function that reverse an array
+
+const Arr1 = ['value1','value2','value3','value4','value5'];
+const Arr2 = Arr1.reverse();
+console.log(Arr2)
+
+
+//Another method
+
+const Value1 = ['value1','value2','value3','value4','value5'];
+const Value2 = [...Value1].reverse();
+console.log(Value2)
