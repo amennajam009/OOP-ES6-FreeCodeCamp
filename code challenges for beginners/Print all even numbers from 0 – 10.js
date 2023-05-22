e9ad8323a0fd54cb9122dac111586189d1fecba8
trench009 <amennajam7@gmail.com>
2023-05-23 01:50:38 +0500

    add

---
+++ code challenges for beginners/Print all even numbers from 0 – 10.js
@@ -0,0 +1,24 @@
+
+//Print All even numbers from 0 to 10
+
+const PrintEvenNumbers = () =>{
+    for(let i=0; i<=10;i++){
+        if(i%2===0){
+            console.log(i)
+        }
+    }
+}
+PrintEvenNumbers();
+
+
+//print All even numbers from 0 to 20
+
+const PrintagainEvenNumber = () =>{
+    for(let i =0 ; i<=20 ; i++){
+        if(i % 2 ===0){
+            console.log(i)
+        }
+    }
+}
+PrintagainEvenNumber()
+
