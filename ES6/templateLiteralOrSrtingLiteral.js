0840345e140c35a15b1683b9696f85e6639cc651
trench009 <amennajam7@gmail.com>
2023-05-23 03:11:00 +0500

    add

---
+++ ES6/templateLiteralOrSrtingLiteral.js
@@ -0,0 +1,12 @@
+// ES6 privide us new feature which is string literal or template literal with the use of this we can interpolluate alot of things 
+// lemme show the example 
+
+const MyObj = {
+    name:'amen',
+    education:'university',
+    age:'20'
+}
+ 
+const greeting = `hello my name is ${MyObj.name}! i'm ${MyObj.age} years Old`
+console.log(greeting)             
+
