506b89b26fc1440cc5f4c250f7e9b86e7e6dcdc2
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-23 22:18:05 +0500

    add

--- code challenges for beginners/Sort an array from lowest to highest.js
+++ code challenges for beginners/Sort an array from lowest to highest.js
@@ -12,7 +12,7 @@ console.log(value2)
 
 
 //Sort an array from larger number to smaller
-
+//this one is not correct
 const Myvalue = [1,2,3,4,5,6,7,8,9,10];
 const solvingMyValue = Myvalue.sort((b,a)=> b-a );
 console.log(solvingMyValue);
@@ -22,3 +22,8 @@ console.log(solvingMyValue);
 const Myothervalue = [10,9,8,7,6,5,4,3,2,1];
 const sortingmyarray = Myothervalue.sort((a,b)=> a-b);
 console.log(sortingmyarray)
+
+// To print From larger to smaller
+const OurArr = [1,2,3,4,5,6,7,8,9,10];
+const OurnewArr = OurArr.sort((a,b)=> b-a);
+console.log(OurnewArr)
