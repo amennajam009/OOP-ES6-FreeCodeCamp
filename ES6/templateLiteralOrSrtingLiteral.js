5637a428b5395590a5c5fa37578a3e3241a33cac
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-23 12:01:30 +0500

    add

--- ES6/templateLiteralOrSrtingLiteral.js
+++ ES6/templateLiteralOrSrtingLiteral.js
@@ -10,3 +10,14 @@ const MyObj = {
 const greeting = `hello my name is ${MyObj.name}! i'm ${MyObj.age} years Old`
 console.log(greeting)             
 
+
+const MyObj1 = {
+    name:'amen',
+    education:'university',
+    age:'20',
+    color:'black',
+      greeting:function(){
+        return `Hi My name is ${MyObj1.name} i'm ${MyObj1.age} years Old . My favourite color is ${MyObj1.color}`
+      }
+}
+console.log(MyObj1.greeting());
