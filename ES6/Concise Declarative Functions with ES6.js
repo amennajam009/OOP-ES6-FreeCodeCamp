3b9cac23693cc1b93a790ade35c8beb2ea897020
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-23 22:26:36 +0500

    add

---
+++ ES6/Concise Declarative Functions with ES6.js
@@ -0,0 +1,24 @@
+// phly jb hum Apny Object k ander method pass karwty thy toh usmay hum function keyword use krty thy lemme show you one example
+
+const Person ={
+    name:'random',
+    class:'random',
+    education:'random',
+    greeting : function(){
+        return `Hi my name is ${Person.name} `
+    }
+}
+console.log(Person.greeting());
+// yeh humra ES5 ka concept thaa 
+
+// ES6 may we can remove this keyword and colon hum direct apna method use krsakty han apny object may 
+
+const AnotherPerson = {
+    name:'string',
+    class:'stirng',
+    phone:'string',
+    greeting (){
+        return `Yoo sup here's my ${AnotherPerson.phone}`
+    }
+}
+console.log(AnotherPerson.greeting());
