86676dec119cde8b089ed36aafe112fea81ef208
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-24 19:59:46 +0500

    add

---
+++ ES6/Use getters and setters to Control Access to an Object.js
@@ -0,0 +1,26 @@
+//You can obtain values from an object and set the value of a property within an object.
+
+class MyObj {
+    constructor(name,age,education){
+        this.name = name,
+        this.age = age,
+        this.education=education
+    }
+
+    get name(){
+        return this.name
+    }
+
+    set name(newName){
+        if(newName === 'string'){
+            this.name = newName
+        }
+        else{
+            return 'please enter string'
+        }
+    }
+}
+
+const myvalues = new MyObj('amen',20,'intermediate');
+console.log(myvalues.name());
+
