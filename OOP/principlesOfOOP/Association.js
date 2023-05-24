163df041e9170e29fde0938387db884ad592d48b
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-24 23:10:17 +0500

    concept of association done

--- OOP/principlesOfOOP/Association.js
+++ OOP/principlesOfOOP/Association.js
@@ -26,3 +26,34 @@ amen.messagewanttogive(bmw)  //output will bt (amen is Driving BMW)
 
 // as you can see here humri dono classes different han but still hum inko connect kr paa rhy han from another class 
 
+
+ 
+//with 3 Classes
+class MySelf {
+    constructor(MyName){
+        this.MyName=MyName
+    }
+    sentenceIwantToMake(othersName,advice){
+        return `${this.MyName} Is Ready To Help ${othersName.nameofPerson} Cuz My Parents Teach Me To ${advice.pieceofAdvice} `
+    }
+}
+class OtherPersonName {
+    constructor(nameofPerson){
+        this.nameofPerson=nameofPerson
+    }
+}
+class Advice {
+    constructor(pieceofAdvice){
+        this.pieceofAdvice=pieceofAdvice
+    }
+}
+const Amen = new MySelf('Amen');
+const EveryOne = new OtherPersonName('Others');
+const GoodAdvice = new Advice ('Help Others 🌸');
+
+Amen.sentenceIwantToMake(EveryOne,GoodAdvice)
+
+
+
+
+
