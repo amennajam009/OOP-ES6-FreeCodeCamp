3da9e1e6630b7ac58e86b51cf3d4de3b9e6cd4ef
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-24 13:10:29 +0500

    add

---
+++ code challenges for beginners/Return a Boolean if a number is divisible by 10.js
@@ -0,0 +1,14 @@
+// Return a Boolean if a number is divisible by 10
+
+
+//20, 40, 50, 170, and 990 are all divisible by 10 because their last digit is zero, 0
+// On the other hand, 21, 34, 127, and 468 are not divisible by 10 since they don't end with zero.
+const Myfunction = (number) =>{
+  if(number %10 ===0){
+    return true
+  }
+  else{
+    return false
+  }
+}
+console.log(Myfunction(20));  // Output true :)
---
+++ index.js
@@ -0,0 +1,9 @@
+const myfunction = (number) =>{
+    if(number %10===0){
+      return true
+    }
+    else{
+        return false
+    }
+}
+console.log(myfunction(21));
