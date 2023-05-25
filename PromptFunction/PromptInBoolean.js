ef679c91682ee42a1d581dec8654df4436a416e1
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-25 16:15:40 +0500

    add

--- PromptFunction/PromptInBoolean.js
+++ PromptFunction/PromptInBoolean.js
@@ -4,6 +4,6 @@
 const MyBooleanfunction = () =>{
     const number = parseInt(prompt('Enter Your Number'));
 
-  return  (number%2===0)? true:false
+  return  (number%2===0)? true + 'Number Is Even': false + 'Number Is Not Even It Is Odd';
 }
 console.log(MyBooleanfunction())
