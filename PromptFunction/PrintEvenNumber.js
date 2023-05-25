ed9351b39bb5f1d9870228df556e49f276162022
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-25 16:05:28 +0500

    ADD

--- PromptFunction/PrintEvenNumber.js
+++ PromptFunction/PrintEvenNumber.js
@@ -21,3 +21,44 @@ console.log(PrintEvenNumber());
 
 
 
+
+//what if i want to print odd numbers 
+
+const PrintOddNumber = () =>{
+    const number = parseInt(prompt('Enter Your Number'));
+    const Result = []
+    if(!!number){
+        for(i=0;i<=number;i++){
+            if(i%2 !==0){
+                Result.push(i)
+            }
+        }
+        return Result
+    }
+    else{
+        return 'Enter Valid Number'
+    }
+}
+console.log(PrintOddNumber())
+
+
+
+
+//again pratice
+
+const mypraticefunction = () =>{
+    const  number = parseInt(prompt('Enter Your Number'));
+    const Result =[]
+    if(!!number){
+        for(i=0;i<=number;i++){
+            if(i%2==0){
+                Result.push(i)
+            }
+        }
+        return Result
+    }
+    else{
+        return "Enter Valid NUMBER"
+    }
+ }
+ console.log(mypraticefunction());
