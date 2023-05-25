137cbcb62c43ff339edd4bd00612e48b4010454c
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-25 16:12:58 +0500

    add

---
+++ PromptFunction/PromptInBoolean.js
@@ -0,0 +1,9 @@
+// Whenever i Enter  Even Value in prompt so it should give me output in Boolean
+
+
+const MyBooleanfunction = () =>{
+    const number = parseInt(prompt('Enter Your Number'));
+
+  return  (number%2===0)? true:false
+}
+console.log(MyBooleanfunction())
