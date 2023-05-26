28e7999a7973464d6382ce59f573c1243c436818
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-26 17:30:01 +0500

    add

--- code challenges for beginners/Return the number of vowels in a string.js
+++ code challenges for beginners/Return the number of vowels in a string.js
@@ -15,3 +15,23 @@ const MyString = (str) =>{
 const sentence = "hello, My Name is Amen ";
 const vowelResult = MyString(sentence);
 console.log(`The sentence "${sentence}" contains ${vowelResult} vowels`)
+
+
+
+//again pratice 
+
+const Mynewstringfunction = (str) => {
+    const myString = ['a', 'e', 'i', 'o', 'u'];
+    let ResultOfString = 0;
+    for (let char of str.toLowerCase()) {
+      if (myString.includes(char)) {
+        ResultOfString++;
+      }
+    }
+    return ResultOfString;
+  }
+  
+  const Mysentence = 'Hi, Here we are Testing Our sentence';
+  const testingresults = Mynewstringfunction(Mysentence);
+  console.log(`This Testing Sentence contains ${testingresults} vowels.`);
+
