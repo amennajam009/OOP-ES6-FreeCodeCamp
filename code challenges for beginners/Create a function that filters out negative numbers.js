c2829814d3680650e17497e415da77c4a8617853
Amen Najam <98958807+trench009@users.noreply.github.com>
2023-05-26 15:32:59 +0500

    add

--- code challenges for beginners/Create a function that filters out negative numbers.js
+++ code challenges for beginners/Create a function that filters out negative numbers.js
@@ -7,3 +7,22 @@ return arr.filter((value)=>value >=0);
 const number = [1,2,3,-7,-9,4,5,-10,5,7,-30];
 const showonlypositivevalues = Myfunction(number);
 console.log(showonlypositivevalues)
+
+
+
+
+
+//Antoher way to filterout negative values
+
+const FilterOutNegativenumber = (arr) =>{
+ const Result = []
+ for(let i=0; i<arr.length;i++){
+    if(arr[i]>=0){
+        Result.push(arr[i])
+    }
+ }
+ return Result
+}
+const number2 = [1,2,3,5,7,-5,-7,-3,2,3,4,5]
+const OutPutResult = FilterOutNegativenumber(number2);
+console.log(OutPutResult);
