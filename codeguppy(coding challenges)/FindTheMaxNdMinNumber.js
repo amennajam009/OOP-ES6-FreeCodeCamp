7c943f9b5de64decaea5192bc1ac5b35824ae34e
trench009 <amennajam7@gmail.com>
2023-05-30 00:42:15 +0500

    add

---
+++ codeguppy(coding challenges)/FindTheMaxNdMinNumber.js
@@ -0,0 +1,12 @@
+//We will find the Max number From Array 
+
+const MyArray = [1,3,6,77,8,9,5,5,6,3]
+const FindMaxNumber = Math.max(...MyArray);
+console.log(FindMaxNumber)
+
+
+//We will Find The Min Number From Array
+
+const MyArray2 = [1,2,3,4,5,6,7,4,5,6]
+const FindMinNumber = Math.min(...MyArray2);
+console.log(FindMinNumber)
