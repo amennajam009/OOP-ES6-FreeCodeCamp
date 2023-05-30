d66bf411b7d3c5dd2766edb69984d84cc65d9e61
trench009 <amennajam7@gmail.com>
2023-05-30 23:19:32 +0500

    add

---
+++ PromptFunction/ConvertStringToArray.js
@@ -0,0 +1,12 @@
+// If I input String so it must give me Output In Object
+
+const MyStringCovertToObject =()=>{
+    const EnterYourString = prompt('Enter Your String');
+    let MyString = ''
+    MyString = Array.from(EnterYourString)
+    return MyString
+}
+console.log(MyStringCovertToObject())
+
+
+//Again Doing It
