40d3bed85f0f7163e65c437b5a7bdfc202d9760a
amennajam009 <amennajam7@gmail.com>
2023-07-21 21:07:43 +0500

    add

---
+++ Debugging-Tools/console.log.js
@@ -0,0 +1,6 @@
+// With the use of console.log we can check our output in our console which is located in out network tabs
+
+const myfunction = (value1,value2) =>{
+    return value1+value2
+}
+console.log(10+20)
