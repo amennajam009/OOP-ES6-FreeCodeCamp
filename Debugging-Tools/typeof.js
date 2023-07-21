37fa20bb76eb37bce8203bc6ea59c7373dd15097
amennajam009 <amennajam7@gmail.com>
2023-07-21 23:01:38 +0500

    add

---
+++ Debugging-Tools/typeof.js
@@ -0,0 +1,10 @@
+// The typeof is actually used to see that what type of variable it is actually let suppose we're adding 
+// two number in which one variable is number and other is string so typeof will help us to tell the type 
+// of the variable
+
+const variable1 = 7
+const variable2 = 'Its type is String'
+const variable3 = true
+console.log(typeof variable1)
+console.log(typeof variable2)
+console.log(typeof variable3)
