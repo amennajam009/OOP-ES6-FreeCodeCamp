eeb0fe746e31920413c70b627334bd1f3e7c446e
amennajam009 <amennajam7@gmail.com>
2023-07-21 22:52:01 +0500

    add

--- Debugging-Tools/console.log.js
+++ Debugging-Tools/console.log.js
@@ -3,7 +3,7 @@
 const myfunction = (value1,value2) =>{
     return value1+value2
 }
-console.log(10+20)
+console.log(myfunction(10,20))
 
 const myobj={
     name:'amen',
@@ -11,3 +11,19 @@ const myobj={
     university:'Virtual university'
 }
 console.table(myobj) // console.table will give us result in table
+
+
+//There are alot of console method to use like console.log 
+ 
+const my2function = (num1,num2) =>{
+   return num1+num2
+}
+console.log(my2function)
+
+// We can also use console.clear to clear all the data in our console tab
+
+const testingfunction = (num1,num2) =>{
+    return num1+num2
+}
+console.log(testingfunction(10,20))
+console.clear(testingfunction) // with the use of this we can able to clear our console tab
