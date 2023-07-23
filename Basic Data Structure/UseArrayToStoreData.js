b41d66979e4f02ce356dc39f7f144c25b5f30ad4
amennajam009 <amennajam7@gmail.com>
2023-07-23 16:09:50 +0500

    add

--- Basic Data Structure/UseArrayToStoreData.js
+++ Basic Data Structure/UseArrayToStoreData.js
@@ -4,6 +4,31 @@
 // 1:one-dimensional array
 // 2:multi-dimensional array
 
+
+
+// One-dimensional array
 const OneDimensionalArray = ['one' , 2 , 'Pink' , true , false, 7, null]
 console.log(OneDimensionalArray.length)
+
+
+//Multi-dimensional array
+const MultiDimensionalArray = [
+    {
+        'one':1,
+        'two':2
+    },
+    {
+        'Three':3,
+        'Four':4
+    },
+    {
+        'Five':5,
+        'Six':6
+    },
+    {
+        'seven':7,
+        'Eight':8
+    }
+]
+console.log(MultiDimensionalArray)
 
