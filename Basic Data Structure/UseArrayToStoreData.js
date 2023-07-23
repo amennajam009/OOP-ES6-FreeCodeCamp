26b8159e609dc4d3aafe47645c4e741ddf6b65ae
amennajam009 <amennajam7@gmail.com>
2023-07-23 16:05:07 +0500

    add

---
+++ Basic Data Structure/UseArrayToStoreData.js
@@ -0,0 +1,9 @@
+// We use Array to store the Data
+
+//Now There are two types of Array 
+// 1:one-dimensional array
+// 2:multi-dimensional array
+
+const OneDimensionalArray = ['one' , 2 , 'Pink' , true , false, 7, null]
+console.log(OneDimensionalArray.length)
+
