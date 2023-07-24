075ab7128b1810cfb63eff6820f7b71b6fde0394
Muhammad Salman <muhammad.salman21.dev@gmail.com>
2023-07-24 20:57:11 +0500

    add

--- Basic Data Structure/Array.push().js
+++ Basic Data Structure/Array.push().js
@@ -4,13 +4,10 @@ MyArray.push('six','seven')
 console.log(MyArray)
 
 
-
-
 //Array.unshift is actually used to add the items in the starting of the Array
 const MyArray2 = ['three','four','five','six']
 MyArray2.unshift('one','two')
 console.log(MyArray2)
-
 
 
 //Array.pop is actually used to remove the items from last of the Array
