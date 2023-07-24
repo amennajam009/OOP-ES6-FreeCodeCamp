b94c87199e45d4f48508d03c85f5a5c8dfd98b75
Muhammad Salman <muhammad.salman21.dev@gmail.com>
2023-07-24 20:43:50 +0500

    add

--- Basic Data Structure/Array.push().js
+++ Basic Data Structure/Array.push().js
@@ -4,3 +4,11 @@ const MyArray = ['One' ,'Two' ,'three','Four','Five']
 MyArray.push('six','seven')
 console.log(MyArray)
 
+
+
+
+//Array.unshift is actually used to add the items in the starting of the Array
+
+const MyArray2 = ['three','four','five','six']
+MyArray2.unshift('one','two')
+console.log(MyArray2)
