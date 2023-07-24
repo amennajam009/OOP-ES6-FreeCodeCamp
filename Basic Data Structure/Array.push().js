90a0f5289e0d55959c9938cd978c310dbe957260
Muhammad Salman <muhammad.salman21.dev@gmail.com>
2023-07-24 20:40:50 +0500

    add

---
+++ Basic Data Structure/Array.push().js
@@ -0,0 +1,6 @@
+// Array.push is actually used to add the items in the last of the Array 
+
+const MyArray = ['One' ,'Two' ,'three','Four','Five']
+MyArray.push('six','seven')
+console.log(MyArray)
+
