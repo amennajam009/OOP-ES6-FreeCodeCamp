cb6605bb8dfda94cf53a4bbdd1b7d3edb506282f
amennajam009 <amennajam7@gmail.com>
2023-07-25 19:02:09 +0500

    add

--- Basic Data Structure/Array.push().js
+++ Basic Data Structure/Array.push().js
@@ -18,6 +18,7 @@ MyArray3.pop()
 console.log(MyArray3)
 
 
+
 //Array.shift is actually used to remove the items from the starting of the Array
 const MyArray4 = ['one','two','three','four','five','six','seven']
 MyArray4.shift()
